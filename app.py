5d75538ed0af938e577cabafb8a42b7eb847cea9
Farzan Rahmani <farzanrahmani70@gmail.com>
2021-08-22 20:32:31 +0430

    Farzan's fix

--- app.py
+++ app.py
@@ -4,20 +4,16 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-
-<<<<<<< HEAD
 @app.route("/bye")
 def main():
     return "Byeeee, Have Great Time :)"
-=======
-@app.route("/hello")
-def main():
-    return "Hello There..."
->>>>>>> feat2
 
+@app.route("/hello")
+def main1():
+    return "Hello There this is forever sexy Farzan"
 
 @app.route("/")
-def main():
+def main2():
     return "Welcome! to IUST BOOTCAMP SUMMER 2021"
 
 
